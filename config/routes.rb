ac4761d16114f7c90bcdc65b7b76129d2cd568bd
yoann lopez <yo@yoann.co>
2016-05-11 15:49:15 +0200

    good but invalid params

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+  root :to => 'restaurants#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
